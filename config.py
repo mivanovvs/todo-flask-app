405ae3c81988c77b81327a2121d3c105f3cbf8cf
Mikhail Ivanov <44364351+mivanovvs@users.noreply.github.com>
2020-01-26 10:27:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:password@localhost/todo"
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:password@localhost"
 TEMPLATES_AUTO_RELOAD = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
